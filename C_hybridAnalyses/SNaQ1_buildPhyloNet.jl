ec04ee2ca77c66f35daf2b92ec373b103b635b5c
Austin Koontz <austinkoontz11@gmail.com>
2024-09-25 13:57:29 -0500

    Fixing filepaths and variable names, for Julia script building All Tips phylo network

--- C_hybridAnalyses/SNaQ1_buildPhyloNet.jl
+++ C_hybridAnalyses/SNaQ1_buildPhyloNet.jl
@@ -18,13 +18,13 @@ addprocs(10)
 @everywhere using PhyloNetworks 
 
 # Specify filepath to table of concordance factors, and read in
-Clust3_CF_8sp_filepath = "/RAID1/QUTA_TX_RedOaks/HybridAnalyses/SNaQ1/Input/Clust3_SNaQ1_CFtable_8sp.csv"
+Clust3_CF_8sp_filepath = "/RAID1/QUTA_TX_RedOaks/HybridAnalyses/SNaQ/SNaQ1/Input/Clust3_SNaQ1_CFtable_8sp.csv"
 Clust3_CF_8sp = readTableCF(Clust3_CF_8sp_filepath)
 # Specify filepath to the starting topology (a Newick text file), and read in
-Clust3_Tree_filepath = "/RAID1/QUTA_TX_RedOaks/HybridAnalyses/SNaQ1/Input/SNaQ1_PhyloEnts.tre"
+Clust3_Tree_filepath = "/RAID1/QUTA_TX_RedOaks/HybridAnalyses/SNaQ/SNaQ1/Input/SNaQ1_PhyloEnts.tre"
 Clust3_Tree = readTopology(Clust3_Tree_filepath)
 # Specify output directory
-SNaQ1_Out = "/RAID1/QUTA_TX_RedOaks/HybridAnalyses/SNaQ1/Output/Net0_SNaQ1_8sp"
+SNaQ1_Out = "/RAID1/QUTA_TX_RedOaks/HybridAnalyses/SNaQ/SNaQ1/Output/Net0_SNaQ1_8sp"
 # Call SNaQ to build the initial phylogenetic network. The hmax argument is set to 0 because we're initially 
 # exploring no hybridization events. The default for SNaQ is to use 10 runs.
 SNaQ1_Net0 = snaq!(Clust3_Tree,  Clust3_CF_8sp, hmax=0, filename=SNaQ1_Out, seed=123)
