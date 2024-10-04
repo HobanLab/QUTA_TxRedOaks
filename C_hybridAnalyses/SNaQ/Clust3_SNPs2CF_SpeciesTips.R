189e844860c35f5c5a68f62589171145a5094dc7
Austin Koontz <austinkoontz11@gmail.com>
2024-10-04 14:27:33 -0500

    Scripts for initial SNaQ run using species as tips, instead of individuals

---
+++ C_hybridAnalyses/SNaQ/Clust3_SNPs2CF_SpeciesTips.R
@@ -0,0 +1,61 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# %%% QUTA & TEXAS RED OAKS : SNPS2CF SCRIPT: SPECIES TIPS %%%
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# This is script used to convert a phylip file (or potentially a VCF) to a table of 
+# concordance factors (CF), which can then be used by PhyloTools (SNaQ) to generate a phylogenetic
+# network. The CF table is saved as a CSV in the end of the script: it contains concordance factor
+# values for every possible quartet of individuals included in the input VCF file.
+
+# This script is used to build the CF table of "all possible taxa": the table which includes the individuals in 
+# the "base phylogenetic network", but then also all "tardifolia" individuals. The only difference in
+# this script versus the AllTips script is the Imap file: for this run, the Imap file groups samples
+# into species (rather than keeping them as individuals)
+
+library(pegas)
+library(foreach)
+library(doMC)
+library(parallel)
+
+# %%% VARIABLES %%% ----
+# Read in the functions used to convert a phylip file to a CF table
+QUTA_SNPs2CF_wd <- '/RAID1/QUTA_TX_RedOaks/HybridAnalyses/SNPs2CF/GH_Repo_SNPs2CF/functions_v1.6.R'
+source(QUTA_SNPs2CF_wd)
+# Set working directory to folder containing phylip file
+QUTA_SNaQ_wd <- '/RAID1/QUTA_TX_RedOaks/HybridAnalyses/SNPs2CF/SpeciesTips/'
+setwd(QUTA_SNaQ_wd)
+# Set up relevant cores 
+num_cores <- detectCores() - 4 
+
+# Specify filepaths to input phylip and map files, and to output CSV
+INPUT_VCF <- 'Input/QUTA_Clust3_SNPs2CF_SpeciesTips.vcf'
+OUTPUT_phylip <- 'Input/QUTA_Clust3_SNPs2CF_SpeciesTips.phylip'
+INPUT_map <- 'Input/QUTA_Clust3_SNPs2CF_SpeciesTips_Imap.txt'
+OUTPUT_CFtable <- 'Output/Clust3_SNPs2CF_SpeciesTips_CFtable.csv'
+
+# %%% CONVERT VCF TO PHYLIP ----
+# This command only needs to be run once; once run, a phylip file will be written to disk. This
+# phylip file will be the input for the SNPs2CF command (see below). 
+vcf2phylip(vcf.name=INPUT_VCF, total.SNPs=86142, random.phase=T, 
+           output.name=OUTPUT_phylip,  cores=num_cores)
+# NOTE: converting the VCF to a phylip file leads to 2 lines per individual in the PHYLIP 
+# file to be written (one for each haplotype, labeled _0 and _1). The map file therefore also
+# needs to have these two lines per individual specified.
+
+# %%% RUN SNPS2CF COMMAND ----
+# Print starting time
+startTime <- Sys.time() 
+print(paste0('%%% SNPs2CF START: ', startTime))
+
+# In this command, 2 individuals (4 haplotypes) per species are specified in the phylip file. 
+# Because the between.sp.only command is set to TRUE, all of the sampled quartets will only 
+# involve different species (and no more than 1 individual per species). Bootstrap value
+# set to TRUE (default)
+output <- SNPs2CF(seqMatrix=OUTPUT_phylip, ImapName=INPUT_map, between.sp.only=TRUE,
+                  outputName=OUTPUT_CFtable, save.progress=FALSE, cores=num_cores)
+print(paste0('\n', '%%% Completed running the SNPs2CF command!!! %%%'))
+
+# Calculate and print runtime
+endTime <- Sys.time() 
+print(paste0('%%% SNPs2CF END: ', endTime))
+cat(paste0('\n', '%%% TOTAL RUNTIME: ', endTime-startTime))
---
+++ C_hybridAnalyses/SNaQ/SNaQ_SpeciesTips_buildPhyloNet.jl
@@ -0,0 +1,56 @@
+#!/usr/bin/julia
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# %%% BUILDING PHYLOGENETIC NETWORK OF DISTNCT ENTITIES %%%
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# This is a Julia script used for building a phylogenetic network using SNaQ. This network is used to test
+# the likelihood of different phylogenetic placements of Q. tardifolia. The inputs for this script are a 
+# table of concordance factors (CF table), which was built using the SNPs2CF tool, and a starting topology,
+# which was a simple Newick tree outlining the relations between the 5 species being analyzed.
+
+# Read in necessary packages
+using PhyloNetworks
+using PhyloPlots
+using Distributed
+# Generate multiple nodes, and load PhyloNetworks onto those nodes
+addprocs(16)
+@everywhere using PhyloNetworks 
+
+# Specify filepath to table of concordance factors, and read in
+SpeciesTips_CF_filepath = "/RAID1/QUTA_TX_RedOaks/HybridAnalyses/SNaQ/SpeciesTips/Input/Clust3_SNPs2CF_SpeciesTips_CFtable.csv"
+SpeciesTips_CF = readTableCF(SpeciesTips_CF_filepath)
+# Specify filepath to the starting topology (a Newick text file), and read in
+SpeciesTips_Tree_filepath = "/RAID1/QUTA_TX_RedOaks/HybridAnalyses/SNaQ/SpeciesTips/Input/Clust3_SpeciesTips.tre"
+SpeciesTips_Tree = readTopology(SpeciesTips_Tree_filepath)
+# Specify output directory
+SpeciesTips_Net0_Out = "/RAID1/QUTA_TX_RedOaks/HybridAnalyses/SNaQ/SpeciesTips/Output/SpeciesTips_Net0"
+SpeciesTips_Net1_Out = "/RAID1/QUTA_TX_RedOaks/HybridAnalyses/SNaQ/SpeciesTips/Output/SpeciesTips_Net1"
+SpeciesTips_Net2_Out = "/RAID1/QUTA_TX_RedOaks/HybridAnalyses/SNaQ/SpeciesTips/Output/SpeciesTips_Net2"
+
+# ---- RUNNING SNAQ ----
+# Call SNaQ to build the initial phylogenetic network, varying the hmax value. 
+# The default for SNaQ is to use 10 runs.
+# NET 0
+SpeciesTips_Net0 = snaq!(SpeciesTips_Tree,  SpeciesTips_CF, hmax=0, filename=SpeciesTips_Net0_Out, seed=123)
+# Plot the resulting phylogenetic network and save to a PDF file
+using RCall
+R"pdf"("SpeciesTips_Net0.pdf", width=15, height=9);
+plot(SpeciesTips_Net0, :R);
+R"dev.off()";
+
+# NET 1
+SpeciesTips_Net1 = snaq!(SpeciesTips_Tree,  SpeciesTips_CF, hmax=1, filename=SpeciesTips_Net1_Out, seed=456)
+# Plot the resulting phylogenetic network and save to a PDF file
+using RCall
+R"pdf"("SpeciesTips_Net1.pdf", width=15, height=9);
+plot(SpeciesTips_Net1, :R);
+R"dev.off()";
+
+# NET 2
+SpeciesTips_Net2 = snaq!(SpeciesTips_Tree,  SpeciesTips_CF, hmax=2, filename=SpeciesTips_Net2_Out, seed=789)
+# Plot the resulting phylogenetic network and save to a PDF file
+using RCall
+R"pdf"("SpeciesTips_Net2.pdf", width=15, height=9);
+plot(SpeciesTips_Net2, :R);
+R"dev.off()";
