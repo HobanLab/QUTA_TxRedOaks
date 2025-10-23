5f0f8c55188cf8445bce5115ee681dd339d605ce
Austin Koontz <austinkoontz11@gmail.com>
2025-10-23 15:44:23 -0500

    Changes to comments, clarity

--- A_Genotyping/AllSamples_seReadsOnly_duplicateChecks.R
+++ A_Genotyping/AllSamples_seReadsOnly_duplicateChecks.R
@@ -2,14 +2,15 @@
 # %%% QUTA & TEXAS RED OAKS : ANALYZING GENEPOP FILES %%%
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-# This is an exploratory script, for checking out the number of loci that are distinct between replicate samples
+# This is an exploratory script, for checking out the number of loci that are 
+# distinct between replicate samples. This is done for different subsets of the data.
 
 library(adegenet)
-QUTA_TRO_wd <- '/RAID1/QUTA_TX_RedOaks/Genotyping/SNP_Calling/Output/All_seReadsOnly/'
+QUTA_TRO_SE_wd <- '/RAID1/QUTA_TX_RedOaks/Genotyping/SNP_Calling/Output/All_seReadsOnly/'
 
 # %%% R97 DATASET ----
-# Read in genepop file for All samples (147), single-end data only, R97
-R97_genind <- read.genepop(paste0(QUTA_TRO_wd,"pop_R97/populations.snps.gen"))
+# Read in genepop file for All samples (167), single-end data only, R97
+R97_genind <- read.genepop(paste0(QUTA_TRO_SE_wd,"pop_R97/populations.snps.gen"))
 # Duplicates: SYST-MOR-0007278 and SYST-MOR-0007278rpt
 R97_genind@tab[156:157, 1:5]
 # Which alleles are different between replicates
@@ -20,8 +21,8 @@ length(which(R97_genind@tab[156,] != R97_genind@tab[157,]))
 length(which(R97_genind@tab[156,] != R97_genind@tab[157,]))/ncol(R97_genind@tab)
 
 # %%% R98 DATASET ----
-# Read in genepop file for All samples (147), single-end data only, R98
-R98_genind <- read.genepop(paste0(QUTA_TRO_wd,"pop_R98/populations.snps.gen"))
+# Read in genepop file for All samples (167), single-end data only, R98
+R98_genind <- read.genepop(paste0(QUTA_TRO_SE_wd,"pop_R98/populations.snps.gen"))
 # Duplicates: SYST-MOR-0007278 and SYST-MOR-0007278rpt
 R98_genind@tab[156:157, 1:5]
 # Which alleles are different between replicates
